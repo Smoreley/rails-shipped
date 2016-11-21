bfd6e6a8da6224536c3dea30bb03b8d6fb8a37eb
Alberto Modesto <albe.mode.24@gmail.com>
2016-11-21 14:41:42 -0500

    geocode

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,10 +1,10 @@
 class JobsController < ApplicationController
-    
+
     before_action :authenticate_user!
     def index
         @message = "index"
         @jobs = Job.all
-        
+
         if params[:search]
             @jobs = Job.search(params[:search]).order("created_at DESC")
         else
@@ -13,23 +13,23 @@ class JobsController < ApplicationController
     end
 
     def new
-        @message = "new" 
+        @message = "new"
     end
 
     def create
         @message = "create"
         @new_job = Job.new(job_params)
-        
+
         if @new_job.save
             redirect_to url_for(:controller => :jobs, :action => :index)
         else
-            render 'new' 
+            render 'new'
         end
     end
 
     def show
         @message = "show"
-        @job = Job.find(params[:id]) 
+        @job = Job.find(params[:id])
     end
 
     def edit
@@ -40,11 +40,11 @@ class JobsController < ApplicationController
     def update
         @message = "update"
         @job = Job.find(params[:id])
-        
+
         if @job.update_attributes(job_params)
             redirect_to url_for(:controller => :jobs, :action => :index)
         else
-            render 'edit' 
+            render 'edit'
         end
     end
 
@@ -53,17 +53,17 @@ class JobsController < ApplicationController
         Job.delete(params[:id])
         redirect_to url_for(:controller => :jobs, :action => :index)
     end
-    
+
     def workers
         @title = "Worker"
         @job  = Job.find(params[:id])
         @boats = @job.boats.paginate(page: params[:page])
         render 'show_follow'
     end
-    
+
     private
-    
+
     def job_params
-       params.require(:job).permit(:name, :description, :origin, :destination, :cost, :cc) 
+       params.require(:job).permit(:name, :description, :origin, :destination, :latitude, :longitude, :address, :location, :cost, :cc) 
     end
 end
--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,11 +2,17 @@ class Boat < ApplicationRecord
     belongs_to :user
     has_many :jobs, :through => :assignments
     has_many :assignments
-    
+
     validates :name, uniqueness: true, presence: true
     validates :location, presence: true
     validates :ccc, numericality: { :greater_than => 0, :less_than_or_equal_to => 9999}
-    
+
+    geocoded_by :address
+    after_validation :geocode, :if => :address_changed?
+
+    geocoded_by :location
+    after_validation :geocode, :if => :location_changed?
+
     def self.search(search)
         where("name LIKE ?", "%#{search}%")
     end
